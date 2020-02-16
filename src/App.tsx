f2e387542e004c4aa9a9bd4e63e65f052846dc2d
Melanie Rush <merush@microsoft.com>
2020-02-16 11:15:53 -0700

    skeleton code

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <header>
+          <button>travel</button>
+          <button>photography</button>
+          <button>sports</button>
+        </header>
+        <main>
+          <p>I'm the main-content filling the void!</p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </main>
+        <footer>
+          <p>Contact me</p>
+        </footer>
+      </div>
     </div>
   );
 }
+
 
 export default App;
