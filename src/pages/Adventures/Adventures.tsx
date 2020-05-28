1763c7080dadad02279bd97b0f3dc92016cd49e3
Melanie Rush <merush@microsoft.com>
2020-05-27 21:46:29 -0600

    adding grids of images

--- src/pages/Adventures/Adventures.tsx
+++ src/pages/Adventures/Adventures.tsx
@@ -5,16 +5,136 @@ import './Adventures.css';
 function Adventures() {
   return (
     <div className="Main">
-        <div className="wrapper">
-        <div>
-          <h1>Places</h1>
-          <h3>New Zealand</h3>
-          <iframe title="Tongariro Crossing" width="560" height="315" src="https://www.youtube.com/embed/XALp4EHlPuA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-          <iframe title="Franz Josef Glacier" width="560" height="315" src="https://www.youtube.com/embed/gzoum6EGSUA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-          <h3> Boulder</h3>
-          <iframe title="Boulder Tube To Work Day" width="560" height="315" src="https://www.youtube.com/embed/YGMn4vOzBBg"allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        {/* <div className="wrapper">
+          <div>
+            <h1>Places</h1>
+            <h3>New Zealand</h3>
+            <iframe title="Tongariro Crossing" width="560" height="315" src="https://www.youtube.com/embed/XALp4EHlPuA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            <iframe title="Franz Josef Glacier" width="560" height="315" src="https://www.youtube.com/embed/gzoum6EGSUA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            <h3> Boulder</h3>
+            <iframe title="Boulder Tube To Work Day" width="560" height="315" src="https://www.youtube.com/embed/YGMn4vOzBBg"allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+          </div>
+        </div> */}
+    <head>
+
+    </head>
+    <body>
+      <h1>Adventures</h1>
+      <div className="container">
+
+        <div className="gallery-container w-2 h-2">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?colorado" alt="newzealand"></img>
+            </div>
+            <div className="text">New Zealand</div>
+          </div>   
         </div>
+
+        <div className="gallery-container">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?nature" alt="newzealand"></img>
+            </div>
+            <div className="text">Australia</div>
+          </div>   
         </div>
+
+        <div className="gallery-container">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?food" alt="newzealand"></img>
+            </div>
+            <div className="text">National Parks</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?travel" alt="newzealand"></img>
+            </div>
+            <div className="text">Colorado</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-4">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?pets" alt="newzealand"></img>
+            </div>
+            <div className="text">Sports</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-2">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?dog" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container h-3">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?forrest" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-2">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?lake" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-2">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?tree" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-3">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?nationalpark" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container w-2">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?mountain" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        <div className="gallery-container">
+          <div className="gallery-item">
+            <div className="image">
+              <img src="https://source.unsplash.com/1600x900/?boulder" alt="newzealand"></img>
+            </div>
+            <div className="text">People</div>
+          </div>   
+        </div>
+
+        
+         
+      </div>
+    </body>
+       
     </div>
   );
 }
--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -3,13 +3,18 @@ import './Blog.css';
 
 function Blog() {
   return (
-    <div className="Blog">
-        <div className="wrapper">
-            <div className="main">
-                <p>Tech blogs to go here!</p>
-            </div>
-        </div>
+    <div className="Main">
+    <div className="wrapper">
+    <div>
+      <h1>Places</h1>
+      <h3>New Zealand</h3>
+      <iframe title="Tongariro Crossing" width="560" height="315" src="https://www.youtube.com/embed/XALp4EHlPuA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      <iframe title="Franz Josef Glacier" width="560" height="315" src="https://www.youtube.com/embed/gzoum6EGSUA" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      <h3> Boulder</h3>
+      <iframe title="Boulder Tube To Work Day" width="560" height="315" src="https://www.youtube.com/embed/YGMn4vOzBBg"allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
     </div>
+    </div>
+  </div>
   );
 }
 
